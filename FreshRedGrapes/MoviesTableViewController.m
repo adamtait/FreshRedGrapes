1d68ad391d5f989d446b3a873d5fc60ef4cd9027
Adam Tait <adam@adamtait.com>
2014-01-19 23:30:03 -0800

    link MoviesTableViewController + MoviesDetailController with storyboard

--- FreshRedGrapes/MoviesTableViewController.m
+++ FreshRedGrapes/MoviesTableViewController.m
@@ -19,6 +19,17 @@
     self = [super initWithStyle:style];
     if (self) {
         // Custom initialization
+        NSString *name = @"Adam Tait";
+    }
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        // Custom initialization
+        NSString *name = @"Adam Tait";
     }
     return self;
 }
@@ -44,21 +55,19 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    return 1;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"Cell";
+    static NSString *CellIdentifier = @"MoviesDetailCell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     
     // Configure the cell...
